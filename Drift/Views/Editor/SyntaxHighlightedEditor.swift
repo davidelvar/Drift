29fb856aff010f4d0217e89477ec0dc095ca5a63
Oddur Sigurdsson <oddurs@gmail.com>
2026-01-08 15:13:40 -0500

    Fix compiler errors in LineNumberRulerView
    - Change failable init to properly call super.init(coder:)
    - Rename drawRect to draw(_:) to match modern AppKit API

--- Drift/Views/Editor/SyntaxHighlightedEditor.swift
+++ Drift/Views/Editor/SyntaxHighlightedEditor.swift
@@ -21,11 +21,11 @@ class LineNumberRulerView: NSRulerView {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
     
-    override func drawRect(_ dirtyRect: NSRect) {
-        super.drawRect(dirtyRect)
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
         
         guard let textView = textView, let layoutManager = textView.layoutManager else { return }
         
